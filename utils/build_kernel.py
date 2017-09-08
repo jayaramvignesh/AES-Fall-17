cc4ae347f97792c8c980d0c4ec377957c1ef9f53
jayaramvignesh <jayaramvignesh@gmail.com>
2017-09-08 12:34:28 -0600

    modified build script

--- utils/build_kernel.py
+++ utils/build_kernel.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
+
+import os
 import subprocess
 
+os.chdir('/home/vigi/linux-4.12.10')
 subprocess.call("sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc",shell=True,stdout=None)
 
 subprocess.call("cp /boot/config-$(uname -r) .config",shell=True,stdout=None)
 subprocess.call("make oldconfig",shell=True,stdout=None)
 
-subprocess.call("sudo make -j 4 && sudo make modules_install -j 4 && sudo make install -j 4",shell=True,stdout=None)
+subprocess.call("sudo make -j4 && sudo make modules_install -j4 && sudo make install -j4",shell=True,stdout=None)
 
 subprocess.call("sudo update-initramfs -c -k 4.12.10",shell=True,stdout=None)
 
