d7cefedc3808d2062b553d95c45748d0cc014e7e
jayaramvignesh <jayaramvignesh@gmail.com>
2017-10-30 17:12:57 -0600

    adding header file for timer

---
+++ PROJECT_1/Include/timer.h
@@ -0,0 +1,11 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+#include "main.h"
+#include "message.h"
+
+ struct itimerval  setup_timer( uint32_t sec_value, uint32_t usec_value);
+
+  struct timespec heartbeat_setup(uint32_t second_value, uint32_t nanosec_value);
+
+#endif
