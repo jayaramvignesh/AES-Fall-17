3095c4466486d8df0594141a7c053296fe20f45d
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 01:49:10 -0600

    modified sig header file

--- PROJECT_1/Include/sig.h
+++ PROJECT_1/Include/sig.h
@@ -5,16 +5,11 @@
 #include "message.h"
 #include "timer.h"
 
-sigset_t sigmask;
-struct sigaction action;
-
 /*set up handler for control+C*/
 sigset_t sigint_mask;
 struct sigaction sigint_handler;
  
 
-void setup_SIGUSR1();
-void setup_SIGUSR2();
 void setup_SIGINT();
 
 #endif
