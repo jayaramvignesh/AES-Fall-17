d871c22b2f920b9e5b7cf1bcf2dcfbda5e857da7
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 01:46:29 -0600

    modified sig source file

--- PROJECT_1/Source/sig.c
+++ PROJECT_1/Source/sig.c
@@ -1,74 +1,35 @@
 #include "sig.h"
 
-/*sigusr1 handler*/
-void sigusr1_handler()
-{
-  printf("\nIDHAR HUN JHANDLERRRRRRRRRRRR MAIN\n");
-  sleep(2);
-}
-
-/*sigusr1 handler*/
-void sigusr2_handler()
-{
-  printf("\nIDHAR HUN JHANDLERRRRRRRRRRRR MAIN\n");
-  pthread_cond_broadcast(&thread2_cond);
-}
-
-
 /*Signal handler for SIGINT*/
 void signal_handler()
 {
-  exit_flag = 1;
-  count = 10;
   time_t a = time(NULL);
   maintask.current_time = ctime(&a);
   maintask.logged_level = INFO;
   maintask.task_ID = main_task;
-  strcpy(maintask.message, "EXIT GRACEFULLY!!!!!!");
+  strcpy(maintask.message_string,"EXIT GRACEFULLYYY!!!!!!!!!!!");
+  strcpy(maintask.message, " - ");
   maintask.message_length = strlen(maintask.message); 
    
+  pthread_cond_broadcast(&temp_task_cond);
+  pthread_cond_broadcast(&light_task_cond);
+ 
+  usleep(10000);
   /*send the message to the queue and check for success*/
-  if(mq_send(mqdes1,(const char *)&maintask, sizeof(maintask),0) == -1)
+  if(mq_send(main_log_mqdes1,(const char *)&maintask, sizeof(maintask),0) == -1)
   {
     printf("\nERROR: mqsend\n");
+  }
+  else if(main_log_queue_count < 10)
+  {
+    main_log_queue_count++;
+    printf("\nIN SIGINT HANDLER, log_queue_count %d\n",main_log_queue_count);
   }  
   
-  pthread_cond_broadcast(&thread1_cond);
-  pthread_cond_broadcast(&thread2_cond);
-  pthread_cond_broadcast(&thread3_cond);
-  pthread_cond_broadcast(&thread4_cond);
+  exit_flag = 1;
 }
 
-void setup_SIGUSR1()
-{
-  /*set up signal mask to block all in main thread*/
-  sigfillset(&sigmask);
-
-  /*Set up signal handlers for SIGUSR1*/
-  action.sa_flags = 0;
-  action.sa_handler = sigusr1_handler;
-  if((sigaction(SIGUSR1, &action, (struct sigaction *)0)) == -1)
-  {
-    printf("\nERROR IN ADDING SIGINT HANDLER: SIGACTION FAILED\n");
-  }
-}
-
-void setup_SIGUSR2()
-{
-  /*set up signal mask to block all in main thread*/
-  sigfillset(&sigmask);
-
-  /*Set up signal handlers for SIGUSR1*/
-  action.sa_flags = 0;
- 
-  action.sa_handler = sigusr2_handler;
-  if((sigaction(SIGUSR2, &action, (struct sigaction *)0)))
-  {
-    printf("\nERROR IN ADDING SIGINT HANDLER: SIGACTION FAILED\n");
-  }
-
-}
-
+/*function to setup sigint*/
 void setup_SIGINT()
 {
 
@@ -89,3 +50,5 @@ void setup_SIGINT()
   }
 
 }
+
+
