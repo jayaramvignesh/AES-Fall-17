131b686cf778319a3370979d0b63e44ee43825d9
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 01:49:22 -0600

    modified timer header file

--- PROJECT_1/Include/timer.h
+++ PROJECT_1/Include/timer.h
@@ -4,6 +4,8 @@
 #include "main.h"
 #include "message.h"
 
+  void signal_handler();
+
  struct itimerval  setup_timer( uint32_t sec_value, uint32_t usec_value);
 
   struct timespec heartbeat_setup(uint32_t second_value, uint32_t nanosec_value);
