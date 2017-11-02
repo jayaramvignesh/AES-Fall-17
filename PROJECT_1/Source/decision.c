bbb3f6b45cc50973cd7be97d11264ad4e1b9b6cb
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 01:46:18 -0600

    modified decision source file

--- PROJECT_1/Source/decision.c
+++ PROJECT_1/Source/decision.c
@@ -5,22 +5,20 @@ void *decision_function()
 {
   while(1)
   {
-   
-    pthread_cond_broadcast(&main_thread4_cond);
-    char buffer[MSG_SIZE];
-    /*lock the mutex and wait for timer to fire*/
-    pthread_mutex_lock(&thread4_mutex);
-    pthread_cond_wait(&thread4_cond,&thread4_mutex); 
  
     if(exit_flag == 1)
     {
         break;
     }
 
-
+  
+    pthread_cond_broadcast(&main_thread4_cond); 
+    char buffer[MSG_SIZE];
+    /*lock the mutex and wait for timer to fire*/
+    pthread_mutex_lock(&decision_queue_mutex);
     printf("\nInside thread 4\n");  
     /*get the attributes for the queue*/
-    mq_getattr(mqdes2,&attr);
+    mq_getattr(decision_mqdes1,&attr);
     
     /*check if there is any messages in the queue*/
     if(attr.mq_curmsgs != 0)
@@ -32,9 +30,8 @@ void *decision_function()
       while(attr.mq_curmsgs != 0)
       {
         Msg_no++;
-        printf("\nMEESSSSSSSAGEEEEEEEEE%d is :\n",Msg_no);
       
-        int n = mq_receive(mqdes2,(char*)&receiver,MSG_SIZE,NULL);
+        int n = mq_receive(decision_mqdes1,(char*)&receiver,MSG_SIZE,NULL);
       
         /*check for which task*/
         if(receiver.task_ID == 1)
@@ -82,19 +79,17 @@ void *decision_function()
           strcpy(logger_level_string,"INFO");
         }
   
-        mq_getattr(mqdes2,&attr);
+        mq_getattr(decision_mqdes1,&attr);
       }
-
+      
+      decision_queue_count = 0;
     }
     
     /*unlock the mutex and exit*/
-    pthread_mutex_unlock(&thread4_mutex);
- 
-    count = 0;
-
+    pthread_mutex_unlock(&decision_queue_mutex);
+    usleep(150000);
   }
   
-  pthread_mutex_unlock(&thread4_mutex);
   printf("\nEXITTTTTTTTTTTTT THREADDDDDDDDDDDDDD 4\n");
   pthread_exit(NULL);
 
