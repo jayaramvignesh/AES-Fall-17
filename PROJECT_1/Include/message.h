f82e5d6d5b94bb41e3e190d6e07575899e107a7f
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 01:48:59 -0600

    modified messsage header file

--- PROJECT_1/Include/message.h
+++ PROJECT_1/Include/message.h
@@ -5,10 +5,14 @@
 
 #define MSG_SIZE 4096
 #define NO_OF_MGS 10
-#define QUEUE_NAME1 "/queue1"
-#define QUEUE_NAME2 "/queue2"
+#define MAIN_LOG_QUEUE_NAME "/main_log_queue"
+#define TEMP_LOG_QUEUE_NAME "/temp_log_queue"
+#define LIGHT_LOG_QUEUE_NAME "/light_log_queue"
 #define FLAG (O_RDWR | O_CREAT)
+#define DECISION_QUEUE_NAME "/decision_queue"
 #define DATA_SIZE 200
+#define TEMP_REQ_QUEUE_NAME "/temp_req_queue"
+#define LIGHT_REQ_QUEUE_NAME "/light_req_queue"
 
 /*enum for logger level*/
 typedef enum logger_level
@@ -28,26 +32,39 @@ typedef enum task_id
     decision_task = 5
 }tid;
 
+
 /*message structure*/
 typedef struct message_log
 {
   char* current_time;  
   log_level logged_level;
   tid task_ID;
+  char message_string[DATA_SIZE];
   char message[DATA_SIZE];
   uint8_t message_length;
 }m_log;
- 
+
+/*structure for request*/
+typedef struct request_log
+{
+  tid Source_task_ID;
+  tid Destination_task_ID;
+  char command;
+  uint32_t delay;
+}r_log;
+
 /*different structures for all tasks*/
-m_log task1;
-m_log task2;
-m_log task3;
+m_log temp_tk;
+m_log light_tk;
 m_log maintask;
 
 /*message queue*/
 struct mq_attr attr;
-mqd_t mqdes1;
-mqd_t mqdes2;
+mqd_t temp_log_mqdes1, light_log_mqdes1, decision_mqdes1, main_log_mqdes1;
+mqd_t tr_mqdes1 , lr_mqdes1 ;
 
 
-#endif 
+#endif
+
+
+
