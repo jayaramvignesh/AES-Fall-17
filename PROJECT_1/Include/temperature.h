37d193f6138f7efe18d51c6f47fe570914df9678
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 22:08:12 -0600

    adding description to temperature header file

--- PROJECT_1/Include/temperature.h
+++ PROJECT_1/Include/temperature.h
@@ -1,3 +1,16 @@
+/*************************************************************************
+*   Authors: Arundhath Swami and Vignesh Jayaram
+*   date edited: 2nd Nov 2017
+*
+*   file: temperature.h
+*
+*   function prototypes
+*   - temperature_function() function for temperature thread
+*
+*
+*****************************************************************************/
+
+
 #ifndef _TEMPERATURE_H
 #define _TEMPERATURE_H
 
