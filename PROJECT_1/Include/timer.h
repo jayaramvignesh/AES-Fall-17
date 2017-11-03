72746c0de640969dcf4cc95cd694556e9c3e15d6
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 22:08:37 -0600

    Made modifications and added description

--- PROJECT_1/Include/timer.h
+++ PROJECT_1/Include/timer.h
@@ -1,10 +1,25 @@
+/*************************************************************************
+*   Authors: Arundhath Swami and Vignesh Jayaram
+*   date edited: 2nd Nov 2017
+*
+*   file: timer.h
+*
+*   function prototypes
+*   -timer_handler() function for callback when timer expires
+*   - setup_timer() function to setup the timer for periodic firing
+*   - heartbeat_setup() function to setup the time for pthread_codn_wait
+*
+*
+*****************************************************************************/
+
+
 #ifndef _TIMER_H
 #define _TIMER_H
 
 #include "main.h"
 #include "message.h"
 
-  void signal_handler();
+  void timer_handler();
 
  struct itimerval  setup_timer( uint32_t sec_value, uint32_t usec_value);
 
