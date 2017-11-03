e79d08ca7539761758c26d2103e7d9c343269dcc
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 22:09:01 -0600

    adding description to logger header file

--- PROJECT_1/Include/logger.h
+++ PROJECT_1/Include/logger.h
@@ -1,3 +1,17 @@
+/*************************************************************************
+*   Authors: Arundhath Swami and Vignesh Jayaram
+*   date edited: 2nd Nov 2017
+*
+*   file: logger.h
+*
+*   function prototypes
+*   - logger_function() function for logger thread
+*
+*
+*****************************************************************************/
+
+
+
 #ifndef _LOGGER_H
 #define _LOGGER_H
 
