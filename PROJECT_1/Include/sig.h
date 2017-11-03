250544de35e100270ac0fe5ab9f41addd982ae70
jayaramvignesh <jayaramvignesh@gmail.com>
2017-11-02 22:08:31 -0600

    Made modifications and added description

--- PROJECT_1/Include/sig.h
+++ PROJECT_1/Include/sig.h
@@ -1,3 +1,18 @@
+/*************************************************************************
+*   Authors: Arundhath Swami and Vignesh Jayaram
+*   date edited: 2nd Nov 2017
+*
+*   file: sig.h
+*
+*   function prototypes
+*   - signal_handler() function for handling signal SIGINT
+*   - setup_SIGINT() function to setup the signal sIGINT
+*
+*
+*****************************************************************************/
+
+
+
 #ifndef _SIG_H
 #define _SIG_H
 
@@ -9,7 +24,7 @@
 sigset_t sigint_mask;
 struct sigaction sigint_handler;
  
-
+void signal_handler();
 void setup_SIGINT();
 
 #endif
