9fd4257e018588f6d3c55f62530c5fc4ad019a18
jayaramvignesh <jayaramvignesh@gmail.com>
2017-12-13 13:26:30 -0700

    adding sources.mk file

--- PROJECT_2/BeagleboneGreen/Source/sources.mk
+++ PROJECT_2/BeagleboneGreen/Source/sources.mk
@@ -2,4 +2,4 @@ INCLUDE_H = ../Include/
 
 vpath %.h $(INCLUDE_H)
 
-SRCS = main.c timer.c sig.c socket_bbg.c decision.c logger.c
+SRCS = main.c timer.c sig.c uart_task.c decision.c logger.c bbg_uart.c
