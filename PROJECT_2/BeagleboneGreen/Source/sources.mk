cc3220e954f87c6b9c816152da2028fadeeb4aab
jayaramvignesh <jayaramvignesh@gmail.com>
2017-12-14 04:06:59 -0700

    modifying sources.mk file

--- PROJECT_2/BeagleboneGreen/Source/sources.mk
+++ PROJECT_2/BeagleboneGreen/Source/sources.mk
@@ -2,4 +2,4 @@ INCLUDE_H = ../Include/
 
 vpath %.h $(INCLUDE_H)
 
-SRCS = main.c timer.c sig.c uart_task.c decision.c logger.c bbg_uart.c
+SRCS = main.c ERROR_LIB.c BBG_LED.c timer.c sig.c uart_task.c decision.c logger.c bbg_uart.c
